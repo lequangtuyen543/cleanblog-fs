fea00798eaf3b49ff90a8abae774ae37becf9dae
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2026-02-02 18:17:48 +0700

    tạo model post

---
+++ backend/api/v1/models/post.model.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    title: String,
+    subtitle: String,
+    createdBy: Date,
+    createdAt: Date,
+    status: String,
+    content: String,
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: Date,
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Post = mongoose.model("Post", postSchema, "posts");
+
+export default Post;
